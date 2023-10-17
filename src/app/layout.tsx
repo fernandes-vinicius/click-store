cb688ca5b6f7adb935c1c0d99f12d15d546a8c4f
fernandes-vinicius <viniciusfernandes6991@gmail.com>
2023-10-17 13:34:58 -0300

    feat: add auth handler to header component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 
 import { Header } from '@/components/ui/header'
+import { AuthProvider } from '@/providers/auth'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -20,8 +21,10 @@ export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Header />
-        {children}
+        <AuthProvider>
+          <Header />
+          {children}
+        </AuthProvider>
       </body>
     </html>
   )
--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -1,3 +1,6 @@
+'use client'
+
+import { signIn, signOut, useSession } from 'next-auth/react'
 import {
   HomeIcon,
   ListOrderedIcon,
@@ -10,8 +13,20 @@ import {
 import { Button } from './button'
 import { Card } from './card'
 import { Sheet, SheetContent, SheetHeader, SheetTrigger } from './sheet'
+import { Avatar, AvatarFallback, AvatarImage } from './avatar'
+import { Separator } from './separator'
 
 export function Header() {
+  const { data, status } = useSession()
+
+  async function handleLoginClick() {
+    await signIn()
+  }
+
+  async function handleLogoutClick() {
+    await signOut()
+  }
+
   return (
     <Card className="flex items-center justify-between p-[1.875rem]">
       <Sheet>
@@ -20,15 +35,51 @@ export function Header() {
             <MenuIcon />
           </Button>
         </SheetTrigger>
+
         <SheetContent side="left">
           <SheetHeader className="text-left text-lg font-semibold">
             Menu
           </SheetHeader>
 
-          <div className="mt-2 flex flex-col gap-2">
-            <Button variant="outline" className="w-full justify-start gap-2">
-              <LogInIcon size={16} /> Fazer Login
-            </Button>
+          {status === 'authenticated' && data.user && (
+            <div className="flex flex-col">
+              <div className="flex items-center gap-2 py-4">
+                <Avatar>
+                  <AvatarFallback>
+                    {data.user.name?.[0].toUpperCase()}
+                  </AvatarFallback>
+                  {data.user.image && <AvatarImage src={data.user.image} />}
+                </Avatar>
+                <div className="flex flex-col">
+                  <p className="font-medium">{data.user.name}</p>
+                  <p className="text-sm opacity-75">Boas compras!</p>
+                </div>
+              </div>
+
+              <Separator />
+            </div>
+          )}
+
+          <div className="mt-4 flex flex-col gap-2">
+            {status === 'unauthenticated' && (
+              <Button
+                onClick={handleLoginClick}
+                variant="outline"
+                className="w-full justify-start gap-2"
+              >
+                <LogInIcon size={16} /> Fazer Login
+              </Button>
+            )}
+
+            {status === 'authenticated' && (
+              <Button
+                onClick={handleLogoutClick}
+                variant="outline"
+                className="w-full justify-start gap-2"
+              >
+                <LogInIcon size={16} /> Fazer Logout
+              </Button>
+            )}
 
             <Button variant="outline" className="w-full justify-start gap-2">
               <HomeIcon size={16} /> Início
