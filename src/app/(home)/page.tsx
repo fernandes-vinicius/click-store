e096ffc438bcf8c3a064ab99b2b8e8c689fddabd
fernandes-vinicius <viniciusfernandes6991@gmail.com>
2023-10-18 10:39:34 -0300

    feat: add compute total price helper

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,22 +1,35 @@
 import Image from 'next/image'
 
-import { Categories } from './components/categories'
+import { prismaClient } from '@/lib/prisma'
 
-export default function Home() {
+import { Categories } from './components/categories'
+import { ProductList } from './components/product-list'
+
+export default async function Home() {
+  const deals = await prismaClient.product.findMany({
+    where: {
+      discountPercentage: {
+        gt: 0,
+      },
+    },
+  })
+
   return (
-    <main className="p-5">
+    <main className="flex flex-col gap-8 py-8">
       <Image
         src="/banner-home-01.png"
         alt="Até 55% de desconto só esse mês!"
         height={150}
         width={350}
         sizes="100vw"
-        className="h-auto w-full"
+        className="h-auto w-full px-5"
       />
 
-      <div className="mt-8">
+      <div className="px-5">
         <Categories />
       </div>
+
+      <ProductList products={deals} />
     </main>
   )
 }
