39f7d21639b796e3449688856e7205996f161fa5
fernandes-vinicius <viniciusfernandes6991@gmail.com>
2023-10-18 11:08:56 -0300

    feat: add second banner to home page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -19,8 +19,8 @@ export default async function Home() {
       <Image
         src="/banner-home-01.png"
         alt="Até 55% de desconto só esse mês!"
-        height={150}
-        width={350}
+        height={0}
+        width={0}
         sizes="100vw"
         className="h-auto w-full px-5"
       />
@@ -29,7 +29,19 @@ export default async function Home() {
         <Categories />
       </div>
 
-      <ProductList products={deals} />
+      <div>
+        <p className="mb-3 pl-5 font-bold uppercase">Ofertas</p>
+        <ProductList products={deals} />
+      </div>
+
+      <Image
+        src="/banner-home-02.png"
+        alt="Até 55% de desconto em Mouses!"
+        height={0}
+        width={0}
+        sizes="100vw"
+        className="h-auto w-full px-5"
+      />
     </main>
   )
 }
